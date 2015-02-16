ecdccd080ffda56f98431d5a6beb5fc05b13a03a
Farez Vadsaria <farezvad@gmail.com>
2015-02-15 20:58:36 -0800

    Cache & other helpers to handle nextPage requests

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var cheerio = require('cheerio');
 var htmlparser = require('htmlparser2');
 var bookresult = require('./models/bookresult');
 var urls = require('./urls');
+var redis = require('redis');
+var redisClient;
 
 /* Serving the file when user arrives at the app webpage */
 app.get('/', function(req, res){
@@ -14,37 +16,41 @@ app.get('/', function(req, res){
 
 /* List all the events that are valid "on a connection" */
 io.on('connection', function(socket){
-	console.log("A client connected!");
 	io.emit('response', "Connected to a scaling robot!");
-	// On search event
+  // On connection, record client type
 	socket.on('connection', function(msg){
 		console.log('connection: ' + msg);
+    // TODO figure out if cache is alive
+    // record type of client connected by
+    // incrementing vars stored in cache
 	});
 
 	// On message event
 	socket.on('search', function(searchText){
 		io.emit('message', searchText);
-		// Add '+' between words in the search string
-		searchText = searchText.replace(/\s/g, "+");
-		console.log('search: ' + searchText);
-		// Build url
-		var searchQuery = urls.searchQuery+searchText+'&l=en';
-		var simpleSearchQuery = urls.simpleSearchQuery + searchText + '&searchCode=GKEY^*&limitTo=none&recCount=10&searchType=1&page.search.search.button=Search'
 
-		// old school request
-    	request({
+		// Add '+' between words in the search string and save the original search terms
+		if(typeof(searchText) == 'string') {
+      searchText = searchText.replace(/\s/g, "+");
+    } else var searchTextPage = searchText;
+
+		var simpleSearchQuery = buildOriginalSearchUrl(searchText);
+    // If the message is a url, we're using it and scraping the next page
+    if(searchText.indexOf('http') > -1 ) {
+      simpleSearchQuery = searchText;
+    } else socket.emit('next', simpleSearchQuery);
+    console.log(simpleSearchQuery);
+    request({
 		url: simpleSearchQuery,
-		
 		}, function(error, response, body) {
 			if(!error && response.statusCode == 200) {
-				console.log("Successful response");
+				console.log("html received!");
 				var bookresults = parseHtml(body);
 				socket.emit('results', bookresults);
 			} else {
 				console.log("Error is " + error);
 			}
-		}
-		);
+    });
 	});
 
 // On disconnect event
@@ -52,6 +58,25 @@ io.on('connection', function(socket){
 		console.log('user disconnected');
 	});
 });
+
+/* Build search url  */
+function buildOriginalSearchUrl(searchText) {
+  return urls.simpleSearchQuery + searchText + '&searchCode=GKEY^*&limitTo=none&recCount=10&searchType=1&page.search.search.button=Search'
+}
+
+/* Setup cache */
+function setupCache() {
+  if (process.env.REDISCLOUD_URL) {
+   var redisURL = require('url').parse(process.env.REDISCLOUD_URL);
+   redisClient = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+   redisClient.auth(redisURL.auth.split(":")[1]);
+  } else {
+    redisClient = redis.createClient();
+    redisClient.on('error', function (err) {
+      console.log('Error ' + err);
+    });
+  }
+}
 
 /* Search result is in a div named resultListTextCell with 5 lines each corresponding to HoldingsLink, Author, Publisher, CallNumber and Status */
 function parseHtml(body) {
@@ -88,8 +113,13 @@ function parseHtml(body) {
 		bookResults.push(br);
 	});
 
-	console.log(bookResults);
+	//console.log(bookResults);
 	return bookResults;
+}
+
+function getNextUrl(body) {
+  $ = cheerio.load(body);
+  return urls.baseSearchUrl + $('.jumpBarTabRight').find('a').attr('href');
 }
 
 /* Check to see if the dataString contains keyword in the beginning.  */
